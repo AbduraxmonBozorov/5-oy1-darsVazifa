b8708338f198140765c7d0591b0025eb0920f305
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-06-14 09:49:00 +0500

    1-masala

---
+++ js/my.js
@@ -0,0 +1,41 @@
+// 1-masala
+// function capitalizeArray(arg = []) {
+//   let arr = arg.filter((item) => {
+//     return item.length >= 5;
+//   });
+
+//   let arr1 = arr.map((item) => {
+//     item = item.toLowerCase();
+//     item = item.replace(item[0], item[0].toUpperCase());
+//     return item
+//   });
+
+//   return arr1;
+// }
+// console.log(capitalizeArray(['salom', 'assalomu aleyKum', 'xayr']));
+
+// let capitalize = function (arg = []) {
+//   let arr = arg.filter((item) => {
+//       return item.length >= 5;
+//     });
+//     let arr1 = arr.map((item) => {
+//       item = item.toLowerCase();
+//       item = item.replace(item[0], item[0].toUpperCase());
+//       return item
+//     });
+//     return arr1;
+// };
+// console.log(capitalize(['salom', 'assalomu aleyKum', 'xayr']));
+
+// let capitalize = (arg = []) => {
+//     let arr = arg.filter((item) => {
+//         return item.length >= 5;
+//       });
+//       let arr1 = arr.map((item) => {
+//         item = item.toLowerCase();
+//         item = item.replace(item[0], item[0].toUpperCase());
+//         return item
+//       });
+//       return arr1;
+//   };
+//   console.log(capitalize(['salom', 'assalomu aleyKum', 'xayr']));
