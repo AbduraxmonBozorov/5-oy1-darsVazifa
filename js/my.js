1119bfc4606c34ebf90255cdaef75e1ea1654f71
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-06-14 10:17:51 +0500

    3-masala

--- js/my.js
+++ js/my.js
@@ -39,3 +39,64 @@
 //       return arr1;
 //   };
 //   console.log(capitalize(['salom', 'assalomu aleyKum', 'xayr']));
+
+// ------------------------------------------------------------------
+
+// 2-masala
+// function student(arg=[]){
+//     let s=arg.find(item=>{
+//         return item.age > 20;
+//     })
+//     return s.name.toUpperCase();
+// }
+// console.log(student([{name: 'Johon', age: 18}, {name: "alisher", age: 22}]));
+
+// let student = function(arg=[]){
+//     let s=arg.find(item=>{
+//         return item.age > 20;
+//     })
+//     return s.name.toUpperCase();
+// }
+// console.log(student([{name: 'Johon', age: 18}, {name: "alisher", age: 22}]));
+
+// let student = (arg = []) => {
+//   let s = arg.find((item) => {
+//     return item.age > 20;
+//   });
+//   return s.name.toUpperCase();
+// };
+// console.log(
+//   student([
+//     { name: "Johon", age: 18 },
+//     { name: "alisher", age: 22 },
+//   ])
+// );
+
+// ------------------------------------------------------------------------------------
+
+// 3-masala
+// function arraySum(arg = []) {
+//   let arr = arg.filter((item) => {
+//     return item >= 100;
+//   });
+
+//   let sum=0;
+//   arr.forEach(item=>{
+//     sum+=item;
+//   })
+//   return sum;
+// }
+// console.log(arraySum([120, 85, 100, 200, 65]));
+
+// let arraySum = function(arg = []) {
+//     let arr = arg.filter((item) => {
+//       return item >= 100;
+//     });
+  
+//     let sum=0;
+//     arr.forEach(item=>{
+//       sum+=item;
+//     })
+//     return sum;
+//   }
+//   console.log(arraySum([120, 85, 100, 200, 65]));
